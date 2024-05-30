8faac9954adec745d2d34f4510ae6005b6dfe373
Jalen Lum <808jalen@gmail.com>
2024-05-29 19:21:07 -1000

    started signup endpoint

--- server.js
+++ server.js
@@ -1,14 +1,21 @@
-const express = require('express');
-const mysql = require('mysql2');
-const cors = require('cors')
-
+const express = require("express");
+const mysql = require("mysql2");
+const cors = require("cors");
 const app = express();
+
 app.use(cors());
+app.use(express.json());
 
-app.get('/', (req, res) => {
-    return res.json("From backend");
-})
+const port = process.env.PORT || 8080;
 
-app.listen(8081, () => {
-    console.log("listening");
-})
+app.listen(port, () => {
+  console.log(`Email List Rest API listening on port ${port}`);
+});
+
+app.get("/", (req, res) => {
+  res.json("Bowow");
+});
+
+app.get("/signup", async(req, res) => {
+    const query = "SELECT * FROM users WHERE name = ?"
+});
